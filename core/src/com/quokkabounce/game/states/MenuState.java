f0c06b7878ddb6e331dfd926a448eecaf52f50af
Abhi <vijayakumar.abhishek@gmail.com>
2017-08-31 22:47:45 -0500

    no message

--- core/src/com/quokkabounce/game/states/MenuState.java
+++ core/src/com/quokkabounce/game/states/MenuState.java
@@ -3,13 +3,12 @@ package com.quokkabounce.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.quokkabounce.game.QuokkaBounce;
-
 /**
  * Created by Eric on 8/28/2017.
  */
 
 public class MenuState extends State{
+
     private Texture levelSelectBackground;
     private Texture levelButton1;
     public MenuState(GameStateManager gsm) {
@@ -32,8 +31,8 @@ public class MenuState extends State{
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(levelSelectBackground, 0, 0, QuokkaBounce.WIDTH, QuokkaBounce.HEIGHT);
-        sb.draw(levelButton1, (QuokkaBounce.WIDTH / 2) - (levelButton1.getWidth() / 2), (QuokkaBounce.HEIGHT / 2) - (levelButton1.getHeight() / 2));
+        sb.draw(levelSelectBackground, 0, 0, Gdx.graphics.getWidth() , Gdx.graphics.getHeight());
+        sb.draw(levelButton1, Math.round(Gdx.graphics.getWidth() / 2) - (levelButton1.getWidth() / 2), Math.round(Gdx.graphics.getHeight() / 2) - (levelButton1.getHeight() / 2));
         sb.end();
     }
 
