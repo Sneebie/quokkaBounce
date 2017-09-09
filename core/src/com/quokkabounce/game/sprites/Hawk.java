44a87d41eefbacbc7510d75570ad9fcad700da36
Abhi <vijayakumar.abhishek@gmail.com>
2017-09-08 19:34:23 -0500

    hawk is less taxic

--- core/src/com/quokkabounce/game/sprites/Hawk.java
+++ core/src/com/quokkabounce/game/sprites/Hawk.java
@@ -10,9 +10,10 @@ import com.badlogic.gdx.math.Vector3;
  */
 
 public class Hawk {
-    private static final int SPEED = 400;
-    private static final int RADIUS = 30;
-    private static final int ATTACKSPEED = 100;
+    private static final double SPEED = 0.1;
+    private static final double RADIUS = 10;
+    private static final int ATTACKSPEED = 10;
+    private static final int DTSCALE = 62;
 
     private Texture hawkTexture;
     private Rectangle hawkBounds;
@@ -31,9 +32,9 @@ public class Hawk {
 
         alreadySpotted = false;
 
-        posHawk = new Vector2(Math.round(RADIUS * Math.cos(t)) + x, Math.round(RADIUS * Math.sin(t)) + y);
+        posHawk = new Vector2(Math.round(RADIUS * Math.cos(SPEED * t)) + x, Math.round(RADIUS * Math.sin(SPEED * t)) + y);
 
-        velHawk = new Vector2(Math.round(RADIUS * -1 * Math.sin(t)), Math.round(RADIUS * Math.cos(t)));
+        velHawk = new Vector2(Math.round(SPEED * RADIUS * -1 * Math.sin(SPEED * t)), Math.round(SPEED * RADIUS * Math.cos(SPEED * t)));
 
         hawkBounds = new Rectangle(posHawk.x, posHawk.y, hawkTexture.getWidth(), hawkTexture.getHeight());
     }
@@ -56,12 +57,12 @@ public class Hawk {
 
     public void move(boolean spotsQuokka, float dt, Vector3 posQuokka){
         if(!spotsQuokka){
-            t += 1 / dt;
-            if(t>=360){
-                t=0;
+            t += DTSCALE * dt;
+            while(SPEED*t>=2*Math.PI){
+                t-=2*Math.PI/SPEED;
             }
             System.out.println(posHawk.x);
-            posHawk.set(Math.round(RADIUS * Math.cos(t)) + posHawk.x, Math.round(RADIUS * Math.sin(t)) + posHawk.y);
+            posHawk.set(Math.round(RADIUS * Math.cos(SPEED * t)) + posHawk.x, Math.round(RADIUS * Math.sin(SPEED * t)) + posHawk.y);
         }
         else{
             if(!alreadySpotted) {
